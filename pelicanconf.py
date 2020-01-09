68cb08bf1cfb1323f83e6c7ad8dc3e0bec82f980
Kousuke Takeuchi <ktakeuchi@keisuu.co.jp>
2020-01-09 15:39:24 +0900

    add sitemap

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,8 +33,8 @@ DEFAULT_PAGINATION = 10
 
 RELATIVE_URLS = True
 PATH = 'content'
-STATIC_PATHS  = ['site_images', 'images', 'extra/CNAME', 'extra/favicon.ico']
-EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, 'extra/favicon.ico': {'path': 'favicon.ico'},}
+STATIC_PATHS  = ['site_images', 'images', 'extra/CNAME', 'extra/favicon.ico', 'extra/.gitignore']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, 'extra/favicon.ico': {'path': 'favicon.ico'}, 'extra/.gitignore': {'path': 'extra/.gitignore'}}
 
 THEME = "theme/pelican-clean-blog"
 GOOGLE_ANALYTICS = "UA-122761088-2"
@@ -43,4 +43,4 @@ HEADER_COVER = 'site_images/osaka-castle.jpg'
 FAVICON = 'favicon.ico'
 
 PLUGIN_PATHS = ['./plugins']
-PLUGINS = ['render_math']
+PLUGINS = ['render_math', 'sitemap']
